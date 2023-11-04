1ef7f5a33cddf865c7fe1afea3a7819dbb30e5f9
maxc0rd <bruthead@gmail.com>
2023-11-05 01:06:51 +0200

    L10 Homework

--- L10 Homework.py
+++ L10 Homework.py
@@ -3,26 +3,47 @@
 # 1. Написати функцію, яка отримує як параметр ім'я файлу назви інтернет доменів (domains.txt)
 # та повертає їх у вигляді списку рядків (назви повертати без крапки).
 #
-
-def reading_file_as_list(filename):
-    with open(filename.txt) as temp_file:
-        data = temp_file.readlines()
-
-    return data
-
-
-print(reading_file_as_list(domains))
+# result = []
+#
+#
+# def reading_file_as_list(filename):
+#     with open(filename, "r") as temp_file:
+#         data = temp_file.readlines()
+#         for i in data:
+#             result.append(i.strip(".\n"))
+#
+#     return result
+#
+#
+# print(reading_file_as_list("domains.txt"))
 
 # 2. Написати функцію, яка отримує як параметр ім'я файла (names.txt)
 # і повертає список усіх прізвищ із нього.
 # Кожен рядок файлу містить номер, прізвище, країну, кілька (таблиця взята з вікіпедії).
 # Розділювач - символ табуляції "t"
 #
+# result = []
+#
+#
+# def reading_file_only_lastnames(filename):
+#     with open(filename, "r") as temp_file:
+#         data = temp_file.readlines()
+#         for i in data:
+#             result.append(i.split("\t")[1])
+#
+#     return result
+#
+#
+# print(reading_file_only_lastnames("names.txt"))
+
 # 3. Написати функцію, яка отримує у вигляді параметра ім'я файлу (authors.txt) та повертає список
 # словників виду {"date": date}
 # у яких date - це дата з рядка (якщо є),
 # Наприклад [{"date": "1st January 1919"}, {"date": "8th February 1828"}, ...]
 #
+
+
+
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 #
 # 4* (*здавати не обов'язково).
