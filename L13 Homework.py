232768f420929a82b7eb388c6652c8ac15484815
maxc0rd <bruthead@gmail.com>
2023-11-19 23:44:33 +0200

    L13 Homework

--- L13 Homework.py
+++ L13 Homework.py
@@ -40,9 +40,9 @@ class File:
 
         return dict_dates
 
+
 # 1. Ініціалізація класу з одним параметром – ім'я файлу.
 #
-
 
 test_file = File("domains.txt")
 
@@ -69,10 +69,10 @@ print(second_names.reading_file_only_lastnames())
 # словників виду {"date": date} у яких date - це дата з рядка (якщо є),
 # Наприклад [{"date": "1st January 1919"}, {"date": "8th February 1828"}, ...]
 #
+
 date_list = File("authors.txt")
 
 print(date_list.reading_file_dict_dates())
-
 
 # 5* (*здавати не обов'язково).
 # Написати метод екземпляра класу, отримує у вигляді параметра ім'я файлу (authors.txt) та повертає список
